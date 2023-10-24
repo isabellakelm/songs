a4fcca9139766b74cfb0a4f771ddc0b464941c3b
isabellakelm <105492388+isabellakelm@users.noreply.github.com>
2023-10-24 07:58:54 -0300

    efeito e idAudio

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSom;
 
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
@@ -9,10 +9,13 @@ function tocaSomClap(){
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 const listaDeteclas = document.querySelectorAll('.tecla');
-listaDeteclas[0].onclick=tocaSomPom;
+listaDeteclas[0].onclick=tocaSom;
 let contador = 0;
 while(contador < ListaDeteclas.length){
-    listaDeteclas[contador].onclick = tocaSomPom;
+    const efeito = listaDeteclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeteclas[contador].onclick = function() {
+        tocaSom(idAudio)
     contor=contador + 1;
     console.log(contador);
 }
