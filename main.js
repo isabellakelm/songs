859125183e822a89ba6bd7ae35ec75e23392af58
isabellakelm <105492388+isabellakelm@users.noreply.github.com>
2023-10-24 07:34:06 -0300

    contador

--- main.js
+++ main.js
@@ -9,4 +9,11 @@ function tocaSomClap(){
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 const ListaDeteclas = document.querySelectorAll('.tecla');
-ListaDeteclas[0].onclick=tocaSomPom
+ListaDeteclas[0].onclick=tocaSomPom;
+let contador = 0;
+while(contador < 9){
+    ListaDeteclas[contador].onclick = tocaSomPom;
+    contor=contador + 1;
+    console.log(contador);
+}
+
