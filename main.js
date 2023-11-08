c8da10b8f987990ddac30c9daf871473667a598a
isabellakelm <105492388+isabellakelm@users.noreply.github.com>
2023-11-08 11:27:22 -0300

    ok

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
+//criando/declarando a função tocaSom com parâmetro idElementoAudio
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = tocaSom;
+const ListaDeteclas = document.querySelectorAll('.tecla');
+//Criando refeerência variável contador, atribuindo o valor inicial como "0"
+//Ciando o cóigo de repetição WHILE e mudando para FOR
+//enquanto o contador for menos que o comprimento da lista, tocará o som.
+for(let contador = 0;contador < ListaDeteclas.length;contador++){
+    const tecla = ListaDeteclas[contador];
+    const efeito = classList[1]; //chama a clase do indice 1 do som
+    const idAudio = `#som_${efeito}`; //junção com template string
+    tecla.onclick = function() { //função anônima para tocaSom
+        tocaSom(idAudio);
+    }
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-const listaDeteclas = document.querySelectorAll('.tecla');
-listaDeteclas[0].onclick=tocaSom;
-let contador = 0;
-while(contador < ListaDeteclas.length){
-    const efeito = listaDeteclas[contador].classList[1];
-    const idAudio = "#som_"+efeito;
-    listaDeteclas[contador].onclick = function() {
-        tocaSom(idAudio)
-    contor=contador + 1;
-    console.log(contador);
 }
 
